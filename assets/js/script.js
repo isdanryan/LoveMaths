b1b716c40cf8d96aa97804254511f4e5b88fbeb0
isdanryan <isdanryan@gmail.com>
2023-09-22 22:50:57 +0100

    Add fav icon, minor UX improvements

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,11 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         })
     }
+    document.getElementById("answer-box").addEventListener("keydown", function(event){
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    })
     runGame("addition");
 })
 
@@ -22,6 +27,9 @@ document.addEventListener("DOMContentLoaded", function() {
  * and when the user's answer has been processed
  */
 function runGame(gameType){
+
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();  
     // Create random numbers
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
@@ -49,8 +57,7 @@ function checkAnswer() {
     } else {
         alert(`Aww, that's wrong. You put ${userAnswer}, but the correct answer is ${calculatedAnswer[0]}.`);
         incrementWrongAnswer();
-    }
-    document.getElementById("answer-box").value = null;    
+    }  
 }
 
 function calculateCorrectAnswer() {

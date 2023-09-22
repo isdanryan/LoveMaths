0922188197233116c142c81398c74fd11313eed9
isdanryan <isdanryan@gmail.com>
2023-09-22 21:24:52 +0100

    Display addition question to user

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,20 +10,29 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked subimt!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+
+    runGame("addition");
 })
 
 /**
  * The main game loop that is called after the game loads
  * and when the user's answer has been processed
  */
-function runGame(){
+function runGame(gameType){
     // Create random numbers
     let num1 = Math.floor(Math.random() * 25) + 1;
-    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unkown game type: ${gameType}. Aborting!`;
+    }
 }
 
 function checkAnswer() {
@@ -42,7 +51,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(opperand1, opperand2) {
+    document.getElementById("opperand1").textContent = opperand1;
+    document.getElementById("opperand2").textContent = opperand2;
+    document.getElementById("opperator").textContent = "+";
 
 }
 

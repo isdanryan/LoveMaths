9ee9d91f59e3d2de61ee0fae1c32931365cc4e1f
isdanryan <isdanryan@gmail.com>
2023-09-22 21:06:43 +0100

    Add event listeners and function outline

---
+++ assets/js/script.js
@@ -0,0 +1,53 @@
+// Wait for page to load before running the game
+//Get buttons and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked subimt!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        })
+    }
+})
+
+function runGame(){
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
+
+function displayDivisionQuestion() {
+
+}
